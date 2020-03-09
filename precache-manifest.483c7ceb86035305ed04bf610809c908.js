395bd3160e22ef4e792f59b2065d99c5912ada1b
Aaqib Ismail <aaqibism@usc.edu>
2020-03-09 00:03:16 -0700

    Final Build

---
+++ precache-manifest.483c7ceb86035305ed04bf610809c908.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3ca3730fb3262d1fe6369b9067d431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d038e70c08a080b4eca",
+    "url": "/static/css/main.f92253f9.chunk.css"
+  },
+  {
+    "revision": "bd1a8d6be7398ea5935d",
+    "url": "/static/js/2.1321ca04.chunk.js"
+  },
+  {
+    "revision": "65d8a6274ca582a852e80e7d0c66f569",
+    "url": "/static/js/2.1321ca04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d038e70c08a080b4eca",
+    "url": "/static/js/main.867c645e.chunk.js"
+  },
+  {
+    "revision": "c66653f7beccbbe77079",
+    "url": "/static/js/runtime-main.a7ee45c1.js"
+  },
+  {
+    "revision": "7c7adde0213325e31d6542e1758b1401",
+    "url": "/static/media/home1.7c7adde0.jpg"
+  },
+  {
+    "revision": "bfb4a450dda73c1cceb05d0bcd57061d",
+    "url": "/static/media/logo5.bfb4a450.png"
+  }
+]);
